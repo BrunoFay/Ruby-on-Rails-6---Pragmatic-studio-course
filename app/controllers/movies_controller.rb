b65d22f5c88a08bc361aac8bb061f89647caf0ac
bruno <brunofay1@hotmail.com>
2023-01-05 16:57:21 -0300

    NEW/edit-movie add route and methods in controller to edit a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,18 @@ class MoviesController < ApplicationController
       @movie = Film.find(params[:id])
     end
 
+    def edit
+      @movie = Film.find(params[:id])
+    end
+
+    def update
+      @movie = Film.find(params[:id])
+
+      movie_params=params
+        .require(:film)
+        .permit(:name, :description, :price, :rating, :released_on)
+
+        @movie.update(movie_params)
+        redirect_to @movie
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
  root "movies#index"
  get "movies" => "movies#index"
  get "movies/:id" => "movies#show", as:"movie"
+ get "movies/:id/edit" => "movies#edit", as:"film"
+ patch "movies/:id/edit" => "movies#update"
 end
