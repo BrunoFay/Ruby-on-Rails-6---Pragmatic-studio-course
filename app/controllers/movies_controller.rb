f77eed226bb575843fe80b8a44b2b7ba3ba3ce8f
bruno <brunofay1@hotmail.com>
2023-01-05 13:00:16 -0300

    NEW/routes-links add links on header and films names, change default rails page to movie controller and add link in logo page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
- def index
-  @name = "bruno fay"
-  @age = rand(100)
-  @favorite_movies = Film.all
- end
+    def index
+      @name = "bruno fay"
+      @age = rand(100)
+      @favorite_movies = Film.all
+    end
+    def show
+      @movie = Film.find(params[:id])
+    end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -5,5 +5,12 @@ module MoviesHelper
     else
       number_to_currency(movie.price,precision:0)
     end
+
   end
+
+  def movie_year (movie)
+    movie.released_on.strftime("%Y")
+
+  end
+
 end
