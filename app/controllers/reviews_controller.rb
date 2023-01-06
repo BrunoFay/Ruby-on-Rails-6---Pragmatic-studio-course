01e1efbb33e758e6327db7de896e052415fc7f4b
bruno <brunofay1@hotmail.com>
2023-01-06 17:55:24 -0300

    NEW/ One-to-Many: Forms

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,35 @@
 class ReviewsController < ApplicationController
 
+  before_action :set_movie
+
   def index
-    @film = Film.find(params[:film_id])
-    @reviews = @film.reviews
+    @reviews = @movie.reviews
   end
-  
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+   @movie = Film.find(params[:film_id])
+   @review = @movie.reviews.new(review_params)
+    if @review.save
+      redirect_to film_reviews_path(@movie), notice: "Thanks for your review!"
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+    def set_movie
+      @movie = Film.find(params[:film_id])
+    end
+
+    def review_params
+      params.require(:review).permit(:name, :comment, :stars)
+    end
+
+
 end
