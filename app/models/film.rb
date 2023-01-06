c0801a220423a049c23215a41f1429af8b9fda06
bruno <brunofay1@hotmail.com>
2023-01-06 11:19:13 -0300

    REFACT/validations add validates methods in model film

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,15 @@
 class Film < ApplicationRecord
+  validates :title, :released_on, :duration, presence:true
+  validates :description, length: { minimum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+  validates :rating, inclusion: { in: RATINGS }
+
   def self.released
     where("released_on < ?", Time.now).order("released_on desc")
   end
