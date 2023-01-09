92292ad4048da704c3eb9d5c00bcd7845020fda6
bruno <brunofay1@hotmail.com>
2023-01-09 14:20:14 -0300

    NEW/SESSION add session controller and view and start logic to login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_09_124719) do
+ActiveRecord::Schema.define(version: 2023_01_09_145750) do
 
   create_table "films", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2023_01_09_124719) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
   add_foreign_key "reviews", "films"
