9c9e51d71a0ed6b7bc651e12d7bba0cc3ba708b7
bruno <brunofay1@hotmail.com>
2023-01-10 10:01:17 -0300

    REFACT/admin change admin logic

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,4 +1,7 @@
 class FilmsController < ApplicationController
+  before_action :require_signin, except: [:index,:show]
+  before_action :require_admin, except: [:index, :show]
+
     def index
       @favorite_movies = Film.released
     end
